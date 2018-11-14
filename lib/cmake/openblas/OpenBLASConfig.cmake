92618b1551d083b72c5337d4d2e2dcca6af2e804
KangDroid <kangdroid@KangDroidui-MacBookPro.local>
2018-11-14 22:37:54 +0900

    Binary file from HOMEBREW

---
+++ lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.3")
+SET(OpenBLAS_INCLUDE_DIRS @@HOMEBREW_CELLAR@@/openblas/0.3.3/include)
+SET(OpenBLAS_LIBRARIES @@HOMEBREW_CELLAR@@/openblas/0.3.3/lib/libopenblas.dylib)
